98857eda66110c48cf609052ed2a4714b73a568a
aberkow <berkowitz.clarinet@gmail.com>
2016-02-03 15:26:48 -0500

    many changes...

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/*
+1 - click/return on + button sends value from Add It box to Get It box.
+2 - click on Get It item (or drag it to Got It) sends Get It item to Got It
+3 - Find a way to delete unwanted items (maybe an 'x' button?)
+4 - Add accordion for each item to add categories (or tags)
+*/
+
+
+$(document).ready(function() {
+    $('#add').click(function() {
+        //if there's nothing in the .list__new box send an alert 
+        debugger;
+        if ($('.list__new').val().trim().length == 0) {
+            alert('Please add an item to the list');
+        }
+        //else create a new li element and add the item text to it. 
+        else { 
+            
+        }
+    })
+});
+
