a5f07e0507f16dc99c8e679154f58dfbd068481d
aberkow <berkowitz.clarinet@gmail.com>
2016-02-08 14:58:26 -0500

    many jquery improvements

--- js/app.js
+++ js/app.js
@@ -1,24 +1,19 @@
 /*
-1 - click/return on + button sends value from Add It box to Get It box.
-2 - click on Get It item (or drag it to Got It) sends Get It item to Got It
-3 - Find a way to delete unwanted items (maybe an 'x' button?)
-4 - Add accordion for each item to add categories (or tags)
+1 - sort items by class or by dragging
 */
 
 var input = $('.input__new');
+var need = [];
+var have = [];
 
 $(document).ready(function() {
-   $('.input__add').click(function() {
-       if (input.val().trim().length == 0) {
-           alert('Please add an item');
-       }
-       else {
-           $('.list__items').prepend(renderListElement(input.val()));
-            input.val('');
-       }
-   }) 
-   
-   itemChecked();
+   $('.input__add').click(addNewItem); 
+    
+    $('.list__items').click('.check', function() {
+        $(event.target).closest('li.list__items-need').toggleClass('list__items-have');
+    });
+    
+     
 });
 
 function renderListElement(content) {
@@ -26,17 +21,40 @@ function renderListElement(content) {
                 '<span class="material-icons move">more_vert</span>'+
                 '<span class="material-icons check">check_box</span>'+
                 '<p class="list__items-text">'+ content +'</p>'+
-                '<span class="material-icons trash" onclick="trashItem()">delete</span>'+
+                '<span class="material-icons trash">delete</span>'+
             '</li>';
+    
 }
 
-function itemChecked() {
-    $('.list__items').click('.check', function() {
-        $(event.target).closest('li.list__items-need').toggleClass('list__items-have');
-    });
+function addNewItem() {
+    if (input.val().trim().length == 0) {
+           alert('Please add an item');
+       }
+       else {
+           $('.list__items').prepend(renderListElement(input.val()));
+            input.val('');
+           var currentElement = $('.list__items li').first();
+           currentElement.find('.trash').on('click', trashItem);
+           currentElement.find('.check').on('click', callCheckItem);
+       }
+}
+
+function callCheckItem() {
+    $(event.target).html(
+        $(event.target).text() == 'check_box' ? 'check_box_outline_blank' : 'check_box');
+    //terneray operator (look this up).
 }
 
 function trashItem() {
     $(event.target).closest('li.list__items-need').remove();
     $(event.target).closest('li.list__items-have').remove();
 } 
+
+function moveItem() {
+    //move an item to the right place
+    debugger;
+}
+
+function sortItem() {
+    //sort the items
+}
