148f57e0f1f85c1e0af6b3cda333de212109af11
aberkow <berkowitz.clarinet@gmail.com>
2016-02-10 11:08:06 -0500

    items autosort based on class

--- js/app.js
+++ js/app.js
@@ -3,24 +3,23 @@
 */
 
 var input = $('.input__new');
-//var need = [];
-//var have = [];
 
 $(document).ready(function() {
-    $('.input__new').keydown(function(enter) {
+    //use enter key to input items
+    $('input').keydown(function(enter) {
         if (enter.keyCode == 13) {
             enter.preventDefault();
             addNewItem();
         }
     });
-    
+    //click on cart to input items
     $('.input__add').click(addNewItem); 
-    
+    //click on checkbox to toggle class
     $('.list__items').click('.check', function() {
         $(event.target).closest('li.list__items-need').toggleClass('list__items-have');
     });
 });
-
+//create full list item w/ all symbols
 function renderListElement(content) {
     return '<li class="list__items-need">'+
                 '<span class="material-icons move">more_vert</span>'+
@@ -30,7 +29,7 @@ function renderListElement(content) {
             '</li>';
     
 }
-
+//add an item to the list
 function addNewItem() {
     if (input.val().trim().length == 0) {
            alert('Please add an item');
@@ -40,31 +39,25 @@ function addNewItem() {
             input.val('');
            var currentElement = $('.list__items li').first();
            currentElement.find('.trash').on('click', trashItem);
-           currentElement.find('.check').on('click', callCheckItem);
+           currentElement.find('.check').on('click', callCheckItem);//do I need this and the function in $(document).ready?
            currentElement.find('.move').on('mousedown', moveItem);
        }
 }
-/*
-function enterKeyPressed() {
-    input.keypress(function(event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            alert('key pressed');
-        }
-    });
-}
-*/
+//toggle type of box
 function callCheckItem() {
-    $(event.target).html(
-        $(event.target).text() == 'check_box' ? 'check_box_outline_blank' : 'check_box');
-    //terneray operator (look this up).
+    //sort item to top or bottom of list according to class
+    $(event.target).closest('li.list__items-need').appendTo('.list__items');
+    $(event.target).closest('li.list__items-have').prependTo('.list__items');
+    //check or uncheck the box
+    $(event.target).html($(event.target).text() == 'check_box' ? 'check_box_outline_blank' : 'check_box');
+    //terneray operator - a short way of making an if statement.
 }
-
+//remove item from list
 function trashItem() {
     $(event.target).closest('li.list__items-need').remove();
     $(event.target).closest('li.list__items-have').remove();
 } 
-
+//make items sortable by dragging
 function moveItem() {
     $('.list__items').sortable();
     $('.list__items').disableSelection();  
